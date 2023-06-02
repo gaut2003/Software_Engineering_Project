8d2ddb5b6177e2c94a181a0531e5c5fe5e2735b5
Sayan Das <sayandas001s@gmail.com>
2023-06-02 23:33:42 +0530

    improved

--- main.cpp
+++ main.cpp
@@ -6,147 +6,157 @@
 
 using namespace std;
 
-struct Student {
+class Student {
+private:
     string name;
     string marks;
+
+public:
+    Student(const string& name, const string& marks) : name(name), marks(marks) {}
+
+    string getName() const {
+        return name;
+    }
+
+    string getMarks() const {
+        return marks;
+    }
+
+    void setMarks(const string& newMarks) {
+        marks = newMarks;
+    }
 };
 
-void storeMarks(const string& filename) {
-    ofstream file(filename, ios::app);
-    if (!file) {
-        cout << "Error: Could not open file '" << filename << "'." << endl;
-        return;
+class StudentRecord {
+private:
+    vector<Student> students;
+    string filename;
+
+public:
+    StudentRecord(const string& filename) : filename(filename) {
+        readFromFile();
     }
 
-    cout << "Enter student name (or 'q' to quit): ";
-    string name;
-    while (getline(cin, name)) {
-        if (name == "q")
-            break;
-
-        cout << "Enter marks obtained: ";
-        string marks;
-        getline(cin, marks);
-
-        file << name << "," << marks << endl;
+    void storeMarks() {
+        ofstream file(filename, ios::app);
+        if (!file) {
+            cout << "Error: Could not open file '" << filename << "'." << endl;
+            return;
+        }
 
         cout << "Enter student name (or 'q' to quit): ";
+        string name;
+        while (getline(cin, name)) {
+            if (name == "q")
+                break;
+
+            cout << "Enter marks obtained: ";
+            string marks;
+            getline(cin, marks);
+
+            students.emplace_back(name, marks);
+
+            file << name << "," << marks << endl;
+
+            cout << "Enter student name (or 'q' to quit): ";
+        }
+
+        cout << "Data stored successfully." << endl;
+        file.close();
     }
 
-    cout << "Data stored successfully." << endl;
-    file.close();
-}
+    void updateMarks() {
+        displayData();
 
-void updateMarks(const string& filename) {
-    ifstream file(filename);
-    if (!file) {
-        cout << "Error: Could not open file '" << filename << "'." << endl;
-        return;
+        cout << "Enter the number of the student to update marks: ";
+        int studentNum;
+        cin >> studentNum;
+        cin.ignore();
+
+        if (studentNum < 1 || studentNum > students.size()) {
+            cout << "Invalid student number." << endl;
+            return;
+        }
+
+        cout << "Enter the updated marks: ";
+        string newMarks;
+        getline(cin, newMarks);
+
+        students[studentNum - 1].setMarks(newMarks);
+
+        writeToFile();
+
+        cout << "Marks updated successfully." << endl;
     }
 
-    cout << "Current data:" << endl;
-    vector<Student> students;
-    Student student;
-    int lineCount = 0;
-    while (getline(file, student.name, ',')) {
-        getline(file, student.marks);
-        students.push_back(student);
-        ++lineCount;
-    }
-    file.close();
+    void deleteStudent() {
+        displayData();
 
-    if (lineCount == 0) {
-        cout << "No data found in the file." << endl;
-        return;
-    }
+        cout << "Enter the number of the student to delete: ";
+        int studentNum;
+        cin >> studentNum;
+        cin.ignore();
 
-    cout << "No.  Name         Marks" << endl;
-    cout << "------------------------" << endl;
-    for (int i = 0; i < students.size(); ++i) {
-        cout << setw(3) << i + 1 << ".  ";
-        cout << setw(12) << students[i].name << " ";
-        cout << students[i].marks << endl;
+        if (studentNum < 1 || studentNum > students.size()) {
+            cout << "Invalid student number." << endl;
+            return;
+        }
+
+        students.erase(students.begin() + studentNum - 1);
+
+        writeToFile();
+
+        cout << "Student deleted successfully." << endl;
     }
 
-    cout << "Enter the number of the student to update marks: ";
-    int studentNum;
-    cin >> studentNum;
-    cin.ignore();
+    void displayData() const {
+        if (students.empty()) {
+            cout << "No data found in the file." << endl;
+            return;
+        }
 
-    if (studentNum < 1 || studentNum > lineCount) {
-        cout << "Invalid student number." << endl;
-        return;
+        cout << "No.  Name         Marks" << endl;
+        cout << "------------------------" << endl;
+        for (int i = 0; i < students.size(); ++i) {
+            cout << setw(3) << i + 1 << ".  ";
+            cout << setw(12) << students[i].getName() << " ";
+            cout << students[i].getMarks() << endl;
+        }
     }
 
-    cout << "Enter the updated marks: ";
-    string newMarks;
-    getline(cin, newMarks);
+private:
+    void readFromFile() {
+        ifstream file(filename);
+        if (!file) {
+            cout << "Error: Could not open file '" << filename << "'." << endl;
+            return;
+        }
 
-    students[studentNum - 1].marks = newMarks;
-
-    ofstream outputFile(filename);
-    for (const auto& student : students) {
-        outputFile << student.name << "," << student.marks << endl;
-    }
-    outputFile.close();
-
-    cout << "Marks updated successfully." << endl;
-}
-
-void deleteStudent(const string& filename) {
-    ifstream file(filename);
-    if (!file) {
-        cout << "Error: Could not open file '" << filename << "'." << endl;
-        return;
+        string name, marks;
+        while (getline(file, name, ',')) {
+            getline(file, marks);
+            students.emplace_back(name, marks);
+        }
+        file.close();
     }
 
-    cout << "Current data:" << endl;
-    vector<Student> students;
-    Student student;
-    int lineCount = 0;
-    while (getline(file, student.name, ',')) {
-        getline(file, student.marks);
-        students.push_back(student);
-        ++lineCount;
+    void writeToFile() {
+        ofstream file(filename);
+        if (!file) {
+            cout << "Error: Could not open file '" << filename << "'." << endl;
+            return;
+        }
+
+        for (const auto& student : students) {
+            file << student.getName() << "," << student.getMarks() << endl;
+        }
+        file.close();
     }
-    file.close();
-
-    if (lineCount == 0) {
-        cout << "No data found in the file." << endl;
-        return;
-    }
-
-    cout << "No.  Name         Marks" << endl;
-    cout << "------------------------" << endl;
-    for (int i = 0; i < students.size(); ++i) {
-        cout << setw(3) << i + 1 << ".  ";
-        cout << setw(12) << students[i].name << " ";
-        cout << students[i].marks << endl;
-    }
-
-    cout << "Enter the number of the student to delete: ";
-    int studentNum;
-    cin >> studentNum;
-    cin.ignore();
-
-    if (studentNum < 1 || studentNum > lineCount) {
-        cout << "Invalid student number." << endl;
-        return;
-    }
-
-    students.erase(students.begin() + studentNum - 1);
-
-    ofstream outputFile(filename);
-    for (const auto& student : students) {
-        outputFile << student.name << "," << student.marks << endl;
-    }
-    outputFile.close();
-
-    cout << "Student deleted successfully." << endl;
-}
+};
 
 int main() {
     string filename = "marks.txt";
+    StudentRecord record(filename);
 
     while (true) {
         cout << "1. Store marks" << endl;
@@ -160,39 +170,13 @@ int main() {
         getline(cin, choice);
 
         if (choice == "1") {
-            storeMarks(filename);
+            record.storeMarks();
         } else if (choice == "2") {
-            updateMarks(filename);
+            record.updateMarks();
         } else if (choice == "3") {
-            ifstream file(filename);
-            if (!file) {
-                cout << "Error: Could not open file '" << filename << "'." << endl;
-                continue;
-            }
-
-            cout << "Marks data:" << endl;
-            vector<Student> students;
-            Student student;
-            while (getline(file, student.name, ',')) {
-                getline(file, student.marks);
-                students.push_back(student);
-            }
-            file.close();
-
-            if (students.empty()) {
-                cout << "No data found in the file." << endl;
-                continue;
-            }
-
-            cout << "No.  Name         Marks" << endl;
-            cout << "------------------------" << endl;
-            for (int i = 0; i < students.size(); ++i) {
-                cout << setw(3) << i + 1 << ".  ";
-                cout << setw(12) << students[i].name << " ";
-                cout << students[i].marks << endl;
-            }
+            record.displayData();
         } else if (choice == "4") {
-            deleteStudent(filename);
+            record.deleteStudent();
         } else if (choice == "5") {
             break;
         } else {
